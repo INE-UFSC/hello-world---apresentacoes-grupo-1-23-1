312b21a1f2c18931b52fb5540409c85a99eaa869
João Cabral <joaovcabralm@hotmail.com>
2023-03-09 20:44:00 -0300

    alteracao

--- antonio.py
+++
@@ -1,8 +0,0 @@
-def hello():
-	return 'Olá, me chamo Antônio, tenho 18 anos e sou natural de Porto Algre, mesmo tendo vivido grande parte da minha vida em Florianópolis. 
-	Meus Hobbies são: Futebol, música, jogos e sair com meus amigos. Antes de entrar na universidade, não tinha nenhuma experiência na área, e o pouco que sei agora, foi adquirido nas aulas de POO ano passado, e no contato com alguns amigos que fiz no curso.
-	Exatamente pela minha falta de experiência, estou bem ansioso para colocar alguns conceitos aprendidos em prática, e principalemnte, produzir um sistema útil, com interface e gráficos. ' 
-
-
-#escreva aqui a sua apresentação - nome, idade, hobbies, experiência prévia de programação, e o que você espera desta disciplina
-	
---
+++ membro4.py
@@ -0,0 +1,4 @@
+def hello():
+	return """Olá, eu sou o Tiago, tenho 19 anos. Meus hobbies são primeiramente sofrer com o FIGUEIRENSE FUTEBOL CLUBE mas também gosto de música e videogames. 
+    Não tenho experiência em programação além das adiquiridas na UFSC. Nesta disciplina espero aprender a orientar objetos."""
+    #escreva aqui a sua apresentação - nome, idade, hobbies, experiência prévia de programação, e o que você espera desta disciplina
