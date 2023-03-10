c4160f30ac03a2dc35fd78bd394e562e50c1a8d6
EricAlexCardosoFriess <127218582+EricAlexCardosoFriess@users.noreply.github.com>
2023-03-09 21:31:04 -0300

    Apresentação

--- membro2.py
+++ membro2.py
@@ -1,2 +1,2 @@
 def hello():
-	return "Olá, eu sou o eric" #escreva aqui a sua apresentação - nome, idade, hobbies, experiência prévia de programação, e o que você espera desta disciplina
+	return "Olá, eu sou o Eric Alex,tenho 20 anos.Gosto de praticar esportes,jogar videogames e assisitir séries.Como experiencia de programação antes do curso,eu apenas vi um curso básico de python.Por isso,espero que nesse curso eu consigo aprimorar minhas habilidades de programação,produzindo sistemas com interface e graficos"
