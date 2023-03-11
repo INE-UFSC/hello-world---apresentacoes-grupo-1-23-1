0bef7cf830b056da38f28853254e58d0be096cc4
EricAlex <ericxl2002@gmail.com>
2023-03-11 15:55:48 -0300

    Adicionei cidade natal

--- membro2.py
+++ membro2.py
@@ -1,2 +1,2 @@
 def hello():
-	return "Olá, eu sou o Eric Alex,tenho 20 anos.Gosto de praticar esportes,jogar videogames e assisitir séries.Como experiencia de programação antes do curso,eu apenas vi um curso básico de python.Por isso,espero que nesse curso eu consigo aprimorar minhas habilidades de programação,produzindo sistemas com interface e graficos"
+	return "Olá, eu sou o Eric Alex,tenho 20 anos e moro em Belém,Pará.Gosto de praticar esportes,jogar videogames e assisitir séries.Como experiencia de programação antes do curso,eu apenas vi um curso básico de python.Por isso,espero que nesse curso eu consigo aprimorar minhas habilidades de programação,produzindo sistemas com interface e graficos"
